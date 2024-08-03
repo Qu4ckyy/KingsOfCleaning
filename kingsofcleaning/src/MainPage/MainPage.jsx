734a08eabda13f916a4c53db4cb1ede94f7a1014
Quackyy <mateuszmarczyk07@gmail.com>
2024-08-03 14:03:19 +0200

    wstępny szkielet

---
+++ kingsofcleaning/src/MainPage/MainPage.jsx
@@ -0,0 +1,143 @@
+import "./MaingPage.scss"
+
+const MainPage = () => {
+    return (
+      <div className="main-page">
+        <div className="nav-bar">
+            <nav>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+            </nav>
+        </div>
+        <div className="logo">
+            <img src="" alt="KingsOfCleanLogo" />
+        </div>
+        <div className="services">
+            <h1>Nasze usługi</h1>
+            <div>
+                <img src="" alt="" />
+                <h4>Sprzątanie Biur</h4>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h4>Mycie Okien</h4>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h4>Sprzątanie Przedszkoli</h4>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h4>Sprzątanie terenów zewnętrznych</h4>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h4>sprzątanie osiedli</h4>
+            </div>
+            <button className="more">Sprawdź więcej ofert</button>
+        </div>
+        <div className="AboutUs">
+            <img src="" alt="" />
+            <h2>Co musisz o nas wiedzieć?</h2>
+            <ol></ol>
+        </div>
+        <div className="moreServices">
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Sprzątanie biur</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Sprzątanie Przedszkoli</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Pranie wykładzin oraz mebli</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Obłusga salonów samochodowych</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Sprzątanie terenów zewnętrznych</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Sprzątanie osiedli</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Mycie okien i parapetów</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h3>Sprzątanie sklepów</h3>
+                    <button>Sprawdź ofertę</button>
+                </div>
+            </div>
+        </div>
+        <div className="refrences">
+            <div>
+                <h2>cytat</h2>
+                <h2>osoba</h2>
+            </div>
+            <div>
+                <h2>cytat</h2>
+                <h2>osoba</h2>
+            </div>
+            <div>
+                <h2>cytat</h2>
+                <h2>osoba</h2>
+            </div>
+        </div>
+        <div className="contact">
+            <h1>Kontakt</h1>
+            <h1>nazwa firmy</h1>
+            <div>
+                <img src="" alt="" />
+                <h2>ulica</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>telefon</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>nip</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>mail</h2>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+
+export default MainPage
