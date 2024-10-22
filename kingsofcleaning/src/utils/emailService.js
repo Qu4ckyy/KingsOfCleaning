bb11b927b70307836e6f2b87092f2dfe0e0ea28e
Mateusz <u74_matmar_waw@technischools.com>
2024-10-22 14:11:40 +0200

    poprawka email service

--- kingsofcleaning/src/utils/emailService.js
+++ kingsofcleaning/src/utils/emailService.js
@@ -1,15 +1,23 @@
 import emailjs from "emailjs-com";
 
+var templateParams = {
+  to_name: "Odbiorca",
+  from_name: "Nadawca",
+  message: "Wiadomość testowa",
+};
+
 export const sendEmail = (form) => {
   emailjs.init("Nv2FvIaL4S98hz9aA");
-  return emailjs.sendForm("service_xeramsa", "template_njmt4rt", form).then(
-    (result) => {
-      console.log("Wiadomość została wysłana!", result.text);
-      return true;
-    },
-    (error) => {
-      console.error("Błąd podczas wysyłania wiadomości:", error);
-      return false;
-    }
-  );
+  return emailjs
+    .sendForm("service_xeramsa", "template_njmt4rt", templateParams, form)
+    .then(
+      (result) => {
+        console.log("Wiadomość została wysłana!", result.text);
+        return true;
+      },
+      (error) => {
+        console.error("Błąd podczas wysyłania wiadomości:", error);
+        return false;
+      }
+    );
 };
